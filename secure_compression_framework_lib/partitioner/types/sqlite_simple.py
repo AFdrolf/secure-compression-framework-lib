3dc5933fb9a7c5a64e73a0e38594eb39783b02c0
andresfg <andresfg@mit.edu>
2024-10-17 09:54:14 -0400

    Simple parsers (working, but need to clean up

--- secure_compression_framework_lib/partitioner/types/sqlite_simple.py
+++ secure_compression_framework_lib/partitioner/types/sqlite_simple.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+import sys
+sys.path.append(sys.path[0] + '/../../..')
 from secure_compression_framework_lib.partitioner.partitioner import Partitioner
 
 # TODO: we may need to make this code more efficient to deal with large databases.
@@ -27,18 +29,22 @@ class SQLiteSimplePartitioner(Partitioner):
             cur.execute(f"SELECT * FROM {table_name};")
             for row in cur:
                 # TODO: pre-process row to make it compatible with access control policy (maybe just get the primary key)?
-                principal_id = access_control_policy(row)
+                principal_id = access_control_policy(table, row)
+                if principal_id == None:
+                    continue
                 db_bucket_id = partition_policy(principal_id)
 
                 # Create empty SQLite file if it does not exist yet
                 if db_bucket_id not in db_buckets:
                     # TODO: generalize so that user can specify name
-                    db_bucket_path = db_bucket_id + self.db_path
+                    db_bucket_path = str(db_bucket_id) + self.db_path
                     bucket_con = sqlite3.connect(db_bucket_path)
                     bucket_cur = bucket_con.cursor()
                     for table_schema in schema:
-                        bucket_cur.execute(table_schema[0])
-                    db_buckets.append(bucket_con, bucket_cur)
+                        # sqlite_sequence table gets created automatically; error is thrown if created manually
+                        if "sqlite_sequence" not in table_schema[0]: 
+                            bucket_cur.execute(table_schema[0])
+                    db_buckets[db_bucket_id] = (bucket_con, bucket_cur)
                 else:
                     bucket_cur = db_buckets[db_bucket_id][0]
 
@@ -50,3 +56,75 @@ class SQLiteSimplePartitioner(Partitioner):
             bucket_con.commit()
             bucket_con.close()
         con.close()
+
+
+# For testing, delete later
+
+def create_messages_db(db_name):
+    """Create an SQLite database with a messages table."""
+    # Connect to the SQLite database (it will be created if it doesn't exist)
+    conn = sqlite3.connect(db_name)
+
+    # Create a cursor object to execute SQL commands
+    cursor = conn.cursor()
+
+    # Create the messages table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            gid INTEGER,
+            from_me INTEGER,
+            content TEXT
+        )
+    ''')
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+def insert_message(db_name, gid, from_me, content):
+    """Insert a new message into the messages table."""
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        INSERT INTO messages (gid, from_me, content)
+        VALUES (?, ?, ?)
+    ''', (gid, from_me, content))
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    import os
+    import glob
+
+    file_type = "*.db"
+
+    for file_path in glob.glob(file_type):
+        os.remove(file_path)
+
+    db_name = 'messages.db'
+    create_messages_db(db_name)
+
+    # Params: db_name, gid, from_me, content
+    insert_message(db_name, 1, 1, 'Hello, World!')
+    insert_message(db_name, 1, 1, 'Hello, World!')
+    insert_message(db_name, 2, 1, 'Hello, World!')
+    insert_message(db_name, 7, 1, 'Hello, World!')
+    insert_message(db_name, 7, 1, 'Hello, World!')
+    insert_message(db_name, 7, 1, 'Hello, World!')
+
+    partitioner = SQLiteSimplePartitioner(db_name)
+
+    def access(table, row):
+        if table[0]=="messages":
+            return row[1]
+        else:
+            return "metadata"
+    
+    def partition_policy(id):
+        return id
+
+    partitioner.partition(partition_policy, access)
+
--- secure_compression_framework_lib/partitioner/types/xml_simple.py
+++ secure_compression_framework_lib/partitioner/types/xml_simple.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as ET
 
+import sys
+sys.path.append(sys.path[0] + '/../../..')
 from secure_compression_framework_lib.partitioner.partitioner import Partitioner
 
 # TODO: we may need to make this code more efficient to deal with large databases.
 
 
-class SQLiteSimplePartitioner(Partitioner):
+class XMLSimplePartitioner(Partitioner):
     def __init__(self, db_path):
         self.db_path = db_path
 
@@ -19,6 +21,8 @@ class SQLiteSimplePartitioner(Partitioner):
         for xml_element in root.findall(".//*"):
             # TODO: pre-process element to make it compatible with access control policy (maybe just get the primary key)?
             principal_id = access_control_policy(xml_element)
+            if principal_id == None:
+                continue
             db_bucket_id = partition_policy(principal_id)
 
             # Create empty XML file if it does not exist yet
@@ -36,3 +40,88 @@ class SQLiteSimplePartitioner(Partitioner):
             # TODO: generalize so that user can specify name
             db_bucket_path = db_bucket_id + self.db_path
             bucket_tree.write(db_bucket_path, encoding="utf-8", xml_declaration=True)
+
+# For testing, delete later
+import os
+
+def create_messages_xml(file_name):
+    """Create an XML file with a messages structure."""
+    # Create the root element
+    root = ET.Element("messages")
+    
+    # Create an ElementTree object
+    tree = ET.ElementTree(root)
+
+    # Write the XML file
+    with open(file_name, 'wb') as xml_file:
+        tree.write(xml_file)
+
+def add_message_to_xml(file_name, gid, from_me, content):
+    """Add a message entry to the XML file."""
+    # Load existing XML or create a new one if it doesn't exist
+    if os.path.exists(file_name):
+        tree = ET.parse(file_name)
+        root = tree.getroot()
+    else:
+        root = ET.Element("messages")
+
+    # Create a new message element
+    message = ET.Element("message")
+    
+    # Generate a new id based on the current number of messages
+    message_id = len(root.findall("message")) + 1
+    
+    # Set the attributes for the message
+    id_elem = ET.SubElement(message, "id")
+    id_elem.text = str(message_id)
+    
+    gid_elem = ET.SubElement(message, "gid")
+    gid_elem.text = str(gid)
+    
+    from_me_elem = ET.SubElement(message, "from_me")
+    from_me_elem.text = str(from_me)
+    
+    content_elem = ET.SubElement(message, "content")
+    content_elem.text = content
+
+    # Append the new message to the root element
+    root.append(message)
+
+    # Write the updated tree back to the file
+    tree = ET.ElementTree(root)
+    with open(file_name, 'wb') as xml_file:
+        tree.write(xml_file)
+
+
+if __name__ == "__main__":
+    import os
+    import glob
+
+    file_type = "*.xml"
+
+    for file_path in glob.glob(file_type):
+        os.remove(file_path)
+
+    db_name = 'messages.xml'
+    create_messages_xml(db_name)
+
+    # Params: db_name, gid, from_me, content
+    add_message_to_xml(db_name, 1, 1, '1Hello, World!')
+    add_message_to_xml(db_name, 1, 1, '2Hello, World!')
+    add_message_to_xml(db_name, 2, 1, '3Hello, World!')
+    add_message_to_xml(db_name, 7, 1, '4Hello, World!')
+    add_message_to_xml(db_name, 7, 1, '5Hello, World!')
+    add_message_to_xml(db_name, 7, 1, '7Hello, World!')
+
+    partitioner = XMLSimplePartitioner(db_name)
+
+    def access(element):
+        if element.tag == "message":
+            return element.find('gid').text
+        else:
+            return "metadata"
+    
+    def partition_policy(id):
+        return id
+
+    partitioner.partition(partition_policy, access)
