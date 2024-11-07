e06d4ab35b8b194ef07adec5a812492cf122c607
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2024-11-07 11:01:33 -0500

    Make num trials an argument to keypass latency evaluation

--- evaluation/eval_latency_keypass_xml.py
+++ evaluation/eval_latency_keypass_xml.py
@@ -15,9 +15,8 @@ from tests.test_partitioner_xml import example_group_uuid_as_principal_keepass_s
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("output_dir", help="Directory where evaluation files and stats will be output to", type=Path)
+    parser.add_argument("trials", help="Number of trials", type=int)
     args = parser.parse_args()
-
-    trials = 1000
 
     csv_path = args.output_dir / f"timing.csv"
     xml_path = args.output_dir / f"timing.xml"
@@ -27,11 +26,11 @@ if __name__ == "__main__":
     compress_path = args.output_dir / f"timing.xml.gz"
     compress_func = functools.partial(compress_file, xml_path, compress_path)
 
-    compress_time = timeit.timeit("compress_func()", setup="from __main__ import compress_func", number=trials)
+    compress_time = timeit.timeit("compress_func()", setup="from __main__ import compress_func", number=args.trials)
 
     decompress_func = functools.partial(decompress_file, compress_path)
 
-    decompress_time = timeit.timeit("decompress_func()", setup="from __main__ import decompress_func", number=trials)
+    decompress_time = timeit.timeit("decompress_func()", setup="from __main__ import decompress_func", number=args.trials)
 
     partition_path = args.output_dir / f"timing.xml.gz.safe"
 
@@ -42,7 +41,7 @@ if __name__ == "__main__":
         partition_path.write_bytes(partition_compressed_bytes)
 
     safe_compress_time = timeit.timeit(
-        safe_compress_func, setup="from __main__ import safe_compress_func", number=trials
+        safe_compress_func, setup="from __main__ import safe_compress_func", number=args.trials
     )
 
     def safe_decompress_func():
@@ -50,10 +49,10 @@ if __name__ == "__main__":
         decompress_xml_advanced_by_element(b)
 
     safe_decompress_time = timeit.timeit(
-        safe_decompress_func, setup="from __main__ import safe_decompress_func", number=trials
+        safe_decompress_func, setup="from __main__ import safe_decompress_func", number=args.trials
     )
 
-    print(f"Normal compress time: {compress_time/trials*1000}")
-    print(f"Safe compress time: {safe_compress_time/trials*1000}")
-    print(f"Normal decompress time: {decompress_time/trials*1000}")
-    print(f"Safe decompress time: {safe_decompress_time/trials*1000}")
+    print(f"Normal compress time: {compress_time/args.trials*1000}")
+    print(f"Safe compress time: {safe_compress_time/args.trials*1000}")
+    print(f"Normal decompress time: {decompress_time/args.trials*1000}")
+    print(f"Safe decompress time: {safe_decompress_time/args.trials*1000}")
