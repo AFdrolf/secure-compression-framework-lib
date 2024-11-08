e1f430b36701b4716f8eee5b64eba18c03f77172
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2024-11-08 16:20:54 -0500

    Add missing typehint to compress_xml_simple end to end function

--- secure_compression_framework_lib/end_to_end/compress_xml_simple.py
+++ secure_compression_framework_lib/end_to_end/compress_xml_simple.py
@@ -13,7 +13,7 @@ from secure_compression_framework_lib.partitioner.types.xml_simple import XMLSim
 
 
 def compress_xml_simple(
-    xml_file: Path, access_control_policy: Callable[[XMLDataUnit], Principal], partition_policy
+    xml_file: Path, access_control_policy: Callable[[XMLDataUnit], Principal], partition_policy: Callable[[Principal], str]
 ) -> list[bytes]:
     partitioner = XMLSimplePartitioner(xml_file, access_control_policy, partition_policy)
     bucketed_data = partitioner.partition()
